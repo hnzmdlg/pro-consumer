8d29bd0494b24792036f43204998be24eebfbea2
âcaozhijiang123 <2244787866@qq.com>
2019-03-22 11:26:40 +0800

    ss

--- src/main/java/com/ws/bean/Catalog.java
+++ src/main/java/com/ws/bean/Catalog.java
@@ -8,6 +8,33 @@ public class Catalog implements Serializable {
     private Integer id;
     private String mlname;
     private Integer kid;
+    private Integer type;
+    private Integer lls;
+    private  String oktime;
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getLls() {
+        return lls;
+    }
+
+    public void setLls(Integer lls) {
+        this.lls = lls;
+    }
+
+    public String getOktime() {
+        return oktime;
+    }
+
+    public void setOktime(String oktime) {
+        this.oktime = oktime;
+    }
 
     public Integer getId() {
         return id;
